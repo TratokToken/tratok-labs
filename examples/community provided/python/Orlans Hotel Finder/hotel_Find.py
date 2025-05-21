01fc9b9c1fd0ce5cfa6ed4ac95951f43866d9e82
The Tratok Team <42908324+TratokToken@users.noreply.github.com>
2025-05-21 11:22:52 +0400

    Update hotel_Find.py
    Rechecked to ensure working. Changed city from London to Dubai to rest with more results for hotels.

--- examples/community provided/python/Orlans Hotel Finder/hotel_Find.py
+++ examples/community provided/python/Orlans Hotel Finder/hotel_Find.py
@@ -4,7 +4,7 @@ id = "YOUR ID"
 secret = "YOUR SECRET"
 url = "https://developer.tratok.net/data.php"
 data = {"request": "hotelsInCity", "id": id, "secret": secret,
-        "city": "London"}
+        "city": "Dubai"}
 response = requests.post(url, data=data)
 print(data)
 print(response.text)
